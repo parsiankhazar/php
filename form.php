e4f01a81c510c352c5c0057dcf4b85622fd25e4d
parsiankhazar <44160077+parsiankhazar@users.noreply.github.com>
2018-10-15 12:57:14 +0330

    Add files via upload

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="answer.php" method="post">
+            <p>Select your language :</p>
+            <label>PHP</label><input type="checkbox" name="php"><br/>
+            <label>HTML</label><input type="checkbox" name="html"><br/>
+            <input type="submit" name="btn" value="Send">
+        </form>
+    </body>
+</html>
